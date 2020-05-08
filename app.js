6aec7fd93159bb000c403b6b1c52192537fe7726
Kamil Balda <baldakamil@gmail.com>
2020-05-08 13:31:34 +0200

    functions

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(resultConversion === 'as-number'){
-    //   return +result
-    // }
-    // return result.toString();
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "as-text");
-console.log(combinedAges);
-var combinedNames = combine("Kamil ", "Magda ", "as-text");
-console.log(combinedNames);
-var combinedStringAges = combine("30 ", "26 ", "as-number");
-console.log(combinedStringAges);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 4));
+console.log(printResult(add(5, 4)));
+var combineValues;
+combineValues = add;
+// combineValues = printResult !!ERROR!!
+console.log(combineValues(10, 10));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
